7e0830a12fbe17997ea888247792e790ee0c7227
Mikhail Abramishvili <mg@abramishvili.net>
2023-03-23 19:32:56 +0400

    .

--- newsOld.js
+++ newsOld.js
@@ -27,28 +27,37 @@ async function run() {
     for (const url of links.slice(0, 6)) {
         await page.goto(url)
 
+        // получаем картинки новостей
         const images = await page.$$eval('.strip_of_thumbnails a img', as => as.map(a => a.src.replace('preview/', '')))
         
-        // if(images.length) {
-        //     for (const imagefile of images) {
-        //         const imagefileDL = await page.goto(imagefile);
-        //         fs.writeFile('./uploads/' + imagefile.replace(/^.*[\\\/]/, ''), await imagefileDL.buffer(), function(err) {
-        //             if(err) {
-        //                 return console.log(err);
-        //             }
-            
-        //             console.log("The file was saved!");
-        //         })
-        //     }
-        // }
+        // скачиваем картинки новостей
+        if(images.length) {
+            for (const imagefile of images) {
+                http.get(imagefile, res => {
+                    const f = fs.createWriteStream(Path.resolve(__dirname, 'uploads', imagefile.replace('http://127.0.0.1/images/', '')))
+
+                    res.pipe(f)
+
+                    f.on('finish', () => {
+                        f.close()
+                        console.log(`Image downloaded!`)
+                    })
+                })
+                .on('error', err => {
+                    console.log('Error: ', err.message)
+                })
+            }
+        }
 
         // меняем урлы у картинок
         for (var i = 0; i < images.length; i++) {
             images[i] = images[i].replace('http://127.0.0.1/images', '/press/novosti/images');
         }
 
+        // получаем файлы новостей
         const files = await page.$$eval('.files li a', as => as.map(a => a.href))
 
+        // скачиваем файлы новостей
         if(files.length) {
             for (const file of files) {
                 http.get(file, res => {
@@ -72,6 +81,7 @@ async function run() {
             files[i] = files[i].replace('http://127.0.0.1/docs', '/press/novosti/files');
         }
 
+        // записываем данные в массив
         data.push(...await page.$$eval('#center', (newsItem, images, files) => {
             return newsItem.map(i => {
                 return {
@@ -83,13 +93,15 @@ async function run() {
             })
         }, images, files))
 
-        // console.log('news item saved')
+        console.log('news item saved')
     }
 
+    // записываем данные в файл
     fs.writeFile('newsOld.json', JSON.stringify(data), (err) => {
         if(err) throw err
     })
 
+    // закрываем браузер
     await browser.close()
 }
 
